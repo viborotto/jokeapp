e9bc6d14438fc483ee689fb8e52e0f7561298390
viborotto <vittoriaborotto@gmail.com>
2020-05-17 14:09:39 -0300

    Add View Layer with Thymeleaf

--- src/main/java/devitt/springframework/joke/jokeapp/services/JokeServiceImpl.java
+++ src/main/java/devitt/springframework/joke/jokeapp/services/JokeServiceImpl.java
@@ -1,6 +1,7 @@
 package devitt.springframework.joke.jokeapp.services;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -8,6 +9,7 @@ public class JokeServiceImpl implements JokeService {
 
     private final ChuckNorrisQuotes chuckNorrisQuotes;
 
+    @Autowired
     public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
         this.chuckNorrisQuotes = chuckNorrisQuotes;
     }
